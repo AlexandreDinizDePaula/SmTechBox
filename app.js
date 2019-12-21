b46777418ed599ad1b0dacb9ec22aeb2e1675ad3
Alexandre Diniz de Paula <alexandre.dp@outlook.com>
2019-12-20 22:03:06 -0300

    teste4

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const passport = require('passport');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./app/index');
 const principalRouter = require('./routes/principal');
 const loginRouter = require('./routes/login');
 const logoutRouter = require('./routes/logout');
