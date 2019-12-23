7d70237b4ee2b7324cda8ef43f19076ee3525c3d
Alexandre Diniz <54523326+AlexandreDinizDePaula@users.noreply.github.com>
2019-12-22 21:54:25 -0300

    Update chaves.js

--- modules/chaves.js
+++ modules/chaves.js
@@ -1,5 +1,5 @@
 module.exports =  {
-        id: 'AKIAJKTZI67WBMENARQQ',
-        secret: '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',
+        id: 'seu id',
+        secret: 'sua senha',
         bucketName: 'projeto-nodejs'
     };
